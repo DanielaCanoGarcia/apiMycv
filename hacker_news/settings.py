a64398ab31609814caf24742e33f692839dde373
Daniela Cano Garcia <159411509+DanielaCanoGarcia@users.noreply.github.com>
2024-12-12 12:55:28 -0600

    update

--- hacker_news/settings.py
+++ hacker_news/settings.py
@@ -95,12 +95,8 @@ WSGI_APPLICATION = 'hacker_news.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'mycvApi',
-        'USER': 'postgres',
-        'PASSWORD': 'CAGD020425',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 } 
 
